d590e3bbe44570b153c34744070dd635dae43c5b
unknown <rchen.utd@gmail.com>
2021-11-11 04:21:32 +1100

    Add sounds for correct and wrong answers

--- src/components/Game.js
+++ src/components/Game.js
@@ -3,6 +3,8 @@ import styled from 'styled-components';
 import { QUESTIONS } from './constants';
 import { GameStateContext } from './Content';
 import CountdownTimer from './CountdownTimer';
+import correctSound from './../assets/correctSound.wav';
+import wrongSound from './../assets/wrongSound.wav';
 
 const Container = styled.div`
   flex-grow: 1;
@@ -45,8 +47,18 @@ export default function Game() {
   var playerIndex = (turnCount - 1) % playerCount;
   var currPlayer = playerList[playerIndex];
 
+  let correctAudio = new Audio(correctSound);
+  let wrongAudio = new Audio(wrongSound);
+  wrongAudio.volume = 0.1;
+
   function handleCorrect() {
+    correctAudio.play();
     incrementScore();
+    beginNextTurn();
+  }
+
+  function handleWrong() {
+    wrongAudio.play();
     beginNextTurn();
   }
 
@@ -75,7 +87,7 @@ export default function Game() {
       <BigDiv alignment="top">
         <CountdownTimer
           handleCorrect={handleCorrect}
-          handleWrong={beginNextTurn}
+          handleWrong={handleWrong}
         />
       </BigDiv>
     </Container>
